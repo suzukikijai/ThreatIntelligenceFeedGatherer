bf881b56d3743e2bd6984a10c34339bd0436fd57
Sooraj Antony <soorajantony@live.in>
2018-11-16 17:51:26 +0530

    feedParser functionalised

--- feedParser.py
+++ feedParser.py
@@ -3,15 +3,14 @@ from bs4 import BeautifulSoup
 import requests
 import pickle
 
-def squidblacklist():
-	source = requests.get('http://danger.rulez.sk/projects/bruteforceblocker/blist.php').text
+def squidblacklist(srcURL,regPat):
+	source = requests.get(srcURL).text
 	soup = BeautifulSoup(source,'lxml')
 	match = soup.p.text
-	regex = r"^(\d[^\t]+)[^\n]+"
 
 	test_str = match
 	
-	matches = re.finditer(regex, test_str, re.MULTILINE)
+	matches = re.finditer(regPat, test_str, re.MULTILINE)
 	for matchNum, match in enumerate(matches):
 		matchNum = matchNum + 1
 		
@@ -21,3 +20,9 @@ def squidblacklist():
 	with open('bruteforceblocker.pkl', 'wb') as f:
 		pickle.dump(line, f)
 	return  
+
+
+
+def main():
+	squidblacklist()
+	#regex = r"^(\d[^\t]+)[^\n]+"
