8aa054e5f1b1708361aa2bea0dd16c6ec2788ccd
Sooraj Antony <soorajantony@live.in>
2018-11-16 20:23:20 +0530

    code refactored

--- feedParser.py
+++ feedParser.py
@@ -3,21 +3,18 @@ from bs4 import BeautifulSoup
 import requests
 import pickle
 
-def squidblacklist(srcURL,regPat):
+def squidblacklist(srcURL,regPat,targetFile = 'output.pkl'):
 	source = requests.get(srcURL).text
 	soup = BeautifulSoup(source,'lxml')
 	match = soup.p.text
-	targetFile = 'bruteforceblocker.pkl'
-
 	test_str = match
-	
+
 	matches = re.finditer(regPat, test_str, re.MULTILINE)
+
 	for matchNum, match in enumerate(matches):
 		matchNum = matchNum + 1
-		
 		line = re.sub(regPat, r"\1", match.group())
 		print(line)
-
 	with open(targetFile, 'wb') as f:
 		pickle.dump(line, f)
 	return  
@@ -25,18 +22,23 @@ def squidblacklist(srcURL,regPat):
 
 
 def main():
-	with open("patterns_url.csv") as collector:
+	# VAR Declaration
+	sourceCSV = "patterns_url.csv"
+	URL_regex = "^[^,]+"
+	pattern_regex = "[^,]+$"
+
+	with open(sourceCSV) as collector:
 		next(collector)
 		for line in collector: 
 			#print(line)
-			URLMatches = re.findall("^[^,]+",line)
+			URLMatches = re.findall(URL_regex,line)
 			for URLMatched in URLMatches:
 				url = URLMatched
-				print(url)
-			patternMatches = re.findall("[^,]+$",line)
+				# print(url)
+			patternMatches = re.findall(pattern_regex,line)
 			for patternMatched in patternMatches:
 				patternData = patternMatched
-				print(patternData)
+				# print(patternData)
 			squidblacklist(url,patternData)
             
 
