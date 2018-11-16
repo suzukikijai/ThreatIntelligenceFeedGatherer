14f1da4b5db0dce9b7f0a3b3d2eb6fc5676e1cf6
Sooraj Antony <soorajantony@live.in>
2018-11-16 20:42:12 +0530

    logging enabled

--- feedParser.py
+++ feedParser.py
@@ -8,16 +8,18 @@ def squidblacklist(srcURL,regPat,targetFile = 'output.pkl'):
 	soup = BeautifulSoup(source,'lxml')
 	match = soup.p.text
 	test_str = match
-
+	endMesg = "Feed gathering for" + re.sub("https?://([^/]+)[^\n]+",r"\1",srcURL) + " from " + re.sub("^[^\n]+/([^/]+)",r"\1",srcURL)  + " completed."
 	matches = re.finditer(regPat, test_str, re.MULTILINE)
-
-	for matchNum, match in enumerate(matches):
-		matchNum = matchNum + 1
-		line = re.sub(regPat, r"\1", match.group())
-		print(line)
-	with open(targetFile, 'wb') as f:
-		pickle.dump(line, f)
-	return  
+	try:
+		for matchNum, match in enumerate(matches):
+			matchNum = matchNum + 1
+			data = re.sub(regPat, r"\1", match.group())
+			print(data)
+		with open(targetFile, 'wb') as f:
+			pickle.dump(data, f)
+		return endMesg
+	except Exception as e:
+		print(e)
 
 
 
@@ -26,20 +28,23 @@ def main():
 	sourceCSV = "patterns_url.csv"
 	URL_regex = "^[^,]+"
 	pattern_regex = "[^,]+$"
-
-	with open(sourceCSV) as collector:
-		next(collector)
-		for line in collector: 
-			#print(line)
-			URLMatches = re.findall(URL_regex,line)
-			for URLMatched in URLMatches:
-				url = URLMatched
-				# print(url)
-			patternMatches = re.findall(pattern_regex,line)
-			for patternMatched in patternMatches:
-				patternData = patternMatched
-				# print(patternData)
-			squidblacklist(url,patternData)
+	try:
+		with open(sourceCSV) as collector:
+			next(collector)
+			for line in collector: 
+				#print(line)
+				URLMatches = re.findall(URL_regex,line)
+				for URLMatched in URLMatches:
+					url = URLMatched
+					# print(url)
+				patternMatches = re.findall(pattern_regex,line)
+				for patternMatched in patternMatches:
+					patternData = patternMatched
+					# print(patternData)
+				print(squidblacklist(url,patternData))
+	except Exception as e:
+		print(e)
+	return "Completed all feeds"
             
 
-main()
+print(main())
