b20f6f682c24b6d2eb7b2736a4bac82c8ae55cb6
Sooraj Antony <soorajantony@live.in>
2018-11-19 15:57:46 +0530

    file looping and dir based output classification

--- feedParser.py
+++ feedParser.py
@@ -14,40 +14,45 @@ def feedExtractor(srcURL,regPat,targetFile = 'output.pkl'):
 		for matchNum, match in enumerate(matches):
 			matchNum = matchNum + 1
 			data = re.sub(regPat, r"\1", match.group())
-			print(data)
+			# print(data)
 		try:
 			with open(targetFile, 'wb') as f:
 				pickle.dump(data, f)
 			return endMesg
 		except Exception as e:
-			print("Feed extractor write error: " + e)
+			print("Feed extractor write error: " + str(e))
 	except Exception as e:
-		print("Feed regex extractor error:: " + e)
+		print("Feed regex extractor error: " + str(e))
 
 
 
 def main():
 	# VAR Declaration
-	sourceCSV = "patterns_url.csv"
 	URL_regex = "^[^,]+"
 	pattern_regex = "[^,]+$"
-	try:
-		with open(sourceCSV) as collector:
-			next(collector)
-			for line in collector: 
-				#print(line)
-				URLMatches = re.findall(URL_regex,line)
-				for URLMatched in URLMatches:
-					url = URLMatched
-					# print(url)
-				patternMatches = re.findall(pattern_regex,line)
-				for patternMatched in patternMatches:
-					patternData = patternMatched
-					# print(patternData)
-				filename = re.sub("^[^\n]+/([^/]+)",r"\1",url)  +  " - " + re.sub("https?://([^/]+)[^\n]+",r"\1",url) + ".pkl"
-				print(feedExtractor(url,patternData,filename))
-	except Exception as e:
-		print("Main function error: "+e)
+	for x in ["ip","domain"]:
+		try:
+			sourceCSV = x + ".csv"
+			try:
+				with open(sourceCSV) as collector:
+					next(collector)
+					for line in collector: 
+						#print(line)
+						URLMatches = re.findall(URL_regex,line)
+						for URLMatched in URLMatches:
+							url = URLMatched
+							# print(url)
+						patternMatches = re.findall(pattern_regex,line)
+						for patternMatched in patternMatches:
+							patternData = patternMatched
+							# print(patternData)
+						filename = x + "/" + re.sub("^[^\n]+/([^/]+)",r"\1",url)  +  " - " + re.sub("https?://([^/]+)[^\n]+",r"\1",url) + ".pkl"
+						print(filename)
+						print(feedExtractor(url,patternData,filename))
+			except Exception as e:
+				print("Sub function error in "+str(x)+" : "+str(e))
+		except Exception as e:
+			print("Main function error: "+str(e))
 	return "Completed all feeds"
             
 
